9c046cacaa5d885f6f7d18f7aa9e8efafea02870
aelkarou <karoumi14@gmail.com>
2023-11-27 14:30:35 +0100

    project_3

--- declarations.js
+++ declarations.js
@@ -1,12 +1,13 @@
-const escapeStr = "\"`/\\'";
-const arr = [4 , '2'];
+const escapeStr = "\"`/\\'"
+const arr = [4 , '2']
+Object.freeze(arr)
 const obj = {
     str : "zebi",
     num : 0  ,
     bool : true ,
     undef : undefined 
-};
-
+}
+Object.freeze(obj)
 const nested = {
     arr : [4,undefined,'2'],
     obj : {
@@ -14,7 +15,6 @@ const nested = {
         num : 0  ,
         bool : true ,
     }
-};
-Object.freeze(nested);
-Object.freeze(obj);
-Object.freeze(arr);
+}
+Object.freeze(nested)
+
