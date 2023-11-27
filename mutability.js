ce1a3edb6d815c886fed7d8b7490f7f733377741
aelkarou <karoumi14@gmail.com>
2023-11-27 15:46:55 +0100

    project_4

--- mutability.js
+++ mutability.js
@@ -1,6 +1,6 @@
 const clone1 = Object.assign({}, person);
 const clone2 = Object.assign({}, person);
-var samePerson = Object.assign({}, person);
+const samePerson = person;
 Object.freeze(clone1 ,clone2)
 person.age +=1 
 person.country = 'FR'
