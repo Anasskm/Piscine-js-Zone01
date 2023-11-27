2d430f35032e5e4d5b42d54de89208103e1f6341
aelkarou <karoumi14@gmail.com>
2023-11-27 15:23:06 +0100

    project_4

--- mutability.js
+++ mutability.js
@@ -7,8 +7,6 @@ const person = {
 const  clone1 = person
 const  clone2 = person
 Object.freeze(clone1 ,clone2)
-const  samePerson = person
-
 person.age +=1 
-
 person.country = 'FR'
+const  samePerson = person
