43c86472467ecde0b38e8d4f39b346b99322fa64
aelkarou <karoumi14@gmail.com>
2023-11-28 19:00:30 +0100

    min max

---
+++ min-max.js
@@ -0,0 +1,14 @@
+function max(x,y){
+    x,y= parseFloat(x),parseFloat(y)
+    if (x>y) {
+        return x
+    }
+    return y
+}
+function min(x,y){
+    x,y= parseFloat(x),parseFloat(y)
+    if (x>y) {
+        return y
+    }
+    return x
+}
