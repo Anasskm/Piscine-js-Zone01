c0c885f2188137e9df6442603f3824139b4ee700
aelkarou <karoumi14@gmail.com>
2023-11-28 18:29:31 +0100

    p13

---
+++ method-man.js
@@ -0,0 +1,16 @@
+function words(str){
+   return  str.split(' ')
+}
+
+function sentence(arr){
+    return arr.join(' ')
+}
+function yell(str){
+    return str.toUpperCase()
+}
+function whisper(str){
+    return "*"+str.toLowerCase()+"*"
+}
+function capitalize(str){
+    return str.charAt(0).toUpperCase()+str.slice(1).toLowerCase()
+}
