becbaf6ba4ef347e13bd3f040985d47db5462bbc
aelkarou <karoumi14@gmail.com>
2023-11-28 17:07:16 +0100

    p10

---
+++ dog-years.js
@@ -0,0 +1,19 @@
+const age = {
+    earth : 31557600,
+    mercury: 0.2408467 ,
+venus:  0.61519726 ,
+mars:  1.8808158 ,
+jupiter:  11.862615 ,
+saturn:  29.447498 ,
+uranus:  84.016846 ,
+neptune:  164.79132 ,
+    
+}
+function dogYears(planet ,dog){
+    if(palnet == "earth"){
+        let a = (dog /age.earth)*7
+        return a.toFixed(2)
+    }
+    let a = (dog /(age[planet]*age.earth))*7
+    return a.toFixed(2)
+}
