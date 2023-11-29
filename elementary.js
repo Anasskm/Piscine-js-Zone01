b0d8d8ef9cf46905eed9a3ffbae95f5ce48562e8
aelkarou <karoumi14@gmail.com>
2023-11-29 16:39:10 +0100

    qsf

--- elementary.js
+++ elementary.js
@@ -1,13 +1,40 @@
 function multiply(a, b) {
+    if (a<0 && b<0){
+        a ,b= -a,-b
+    }
+    let sign = fale
+    if(a<0){
+        sign = true
+        a = -a
+    }
+    if(b<0){
+        sign = true
+        b = -b
+    }
     let result = 0
     for (let i = 0; i < b; i++) {
         result += a
     }
-    return result
+    if (!sign){
+        return result
+    }
+    return -result
 }
 
 
 function divide(a, b) {
+    if (a<0 && b<0){
+        a ,b= -a,-b
+    }
+    let sign = fale
+    if(a<0){
+        sign = true
+        a = -a
+    }
+    if(b<0){
+        sign = true
+        b = -b
+    }
     if (b === 0) {
         return Infinity
     }
@@ -17,7 +44,9 @@ function divide(a, b) {
         a -= b;
         q++;
     }
-
+    if (sign){
+        return -q
+    }
     return q;
 }
 function modulo(a, b) {
