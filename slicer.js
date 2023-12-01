8852690a260b1ec940932702c6c291ef81a028dd
aelkarou <karoumi14@gmail.com>
2023-12-01 21:28:12 +0100

    slice

--- slicer.js
+++ slicer.js
@@ -1,33 +1,37 @@
-// Define a standalone slice function
 function slice(input, start, end) {
     if (typeof input === 'string') {
-        // If the input is a string
-        return sliceString(input, start, end);
+      // If the input is a string
+      return sliceString(input, start, end);
     } else if (Array.isArray(input)) {
-        // If the input is an array
-        if (start < 0) {
-            start = input.length + start;
-        }
-        if (end < 0) {
-            end = input.length + end;
-        }
-        return sliceArray(input, start, end);
+      // If the input is an array
+      if (start < 0) {
+        start = input.length + start;
+      }
+      if (end < 0) {
+        end = input.length + end;
+      }
+      return sliceArray(input, start, end);
+    } 
+  }
+  
+  // Implement custom slice for strings
+  function sliceString(str, start, end) {
+    if (end === undefined || end > str.length) {
+      end = str.length;
     }
-}
-
-// Implement custom slice for strings
-function sliceString(str, start, end) {
-    if (end === undefined) {
-        end = str.length;
+    if (start < 0) {
+      start = str.length + start;
+    }
+    if (end < 0) {
+      end = str.length + end;
     }
     return str.substring(start, end);
-}
-
-// Implement custom slice for arrays
-function sliceArray(arr, start, end) {
+  }
+  
+  // Implement custom slice for arrays
+  function sliceArray(arr, start, end) {
     if (end === undefined) {
-        end = arr.length;
+      end = arr.length;
     }
     return arr.slice(start, end);
-}
-
+  }
