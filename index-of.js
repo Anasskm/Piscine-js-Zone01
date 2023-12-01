81b8e7097ba2a7a5899595196f3aa12203e37cdc
aelkarou <karoumi14@gmail.com>
2023-12-01 21:21:28 +0100

    indexof

--- index-of.js
+++ index-of.js
@@ -1,33 +1,26 @@
-function indexOf(ar, elem, a = 0) {
-    for (let i = a; i < ar.length; i++) {
-        if (ar[i] === elem) {
-            return i
-        }
+function indexOf(arr, value, startIndex = 0) {
+    for (let i = startIndex; i < arr.length; i++) {
+      if (arr[i] === value) {
+        return i;
+      }
     }
-    return -1
-
-}
-
-
-
-function lastIndexOf(ar, elem, a = ar.length) {
-    for (let i = a - 1; i >= 0; i--) {
-        if (ar[i] === elem) {
-            return i
-        }
+    return -1;
+  }
+  
+  function lastIndexOf(arr, value, startIndex = arr.length - 1) {
+    for (let i = startIndex; i >= 0; i--) {
+      if (arr[i] === value) {
+        return i;
+      }
     }
-    return -1
-
-}
-
-
-
-function includes(ar, elem) {
-    for (let i = 0; i < ar.length; i++) {
-        if (ar[i] === elem) {
-            return true
-        }
+    return -1;
+  }
+  
+  function includes(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === value) {
+        return true;
+      }
     }
-    return false
-
-}
+    return false;
+  }
