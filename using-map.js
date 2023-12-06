a064db96f2e4745f6d41d3ff4e722bc45ffdc1de
aelkarou <karoumi14@gmail.com>
2023-12-06 10:27:31 +0100

    pls work

---
+++ using-map.js
@@ -0,0 +1,34 @@
+function citiesOnly(arr) {
+    const out = arr.map((obj) => obj["city"])
+    return out
+}
+
+
+function upperCasingStates(arr) {
+    const out = arr.map((str) =>{
+        let a = str.split(' ')
+        const mgad = a.map((elem) => elem.charAt(0).toUpperCase() + elem.slice(1).toLowerCase())
+        return mgad.join(' ')
+        
+
+    } 
+
+    )
+    return out
+}
+function fahrenheitToCelsius(far){
+    const out = far.map((elem)=> String(((5/9)*(parseFloat(elem)-32)).toFixed()+"°C")
+    )
+    return out
+}
+function trimTemp(arr) {
+    const out = arr.map(obj=>({
+        ...obj,
+        temperature:obj.temperature.replace(/\s/g, '')
+    }))
+    return out
+}
+function tempForecasts(arr){
+    const out = arr.map((obj)=> String(((5/9)*(parseFloat(obj.temperature)-32)).toFixed()+"°Celsius")+' in '+obj.city+', '+obj.state.charAt(0).toUpperCase() + obj.state.slice(1).toLowerCase())
+    return out
+}
