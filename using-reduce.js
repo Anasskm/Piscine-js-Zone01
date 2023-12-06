962a47296fb9d387efef63ac6ee589d54df75455
aelkarou <karoumi14@gmail.com>
2023-12-06 19:14:50 +0100

    nadi

--- using-reduce.js
+++ using-reduce.js
@@ -1,10 +1,10 @@
-const adder = (arr, i = arr.length > 0 ? arr[0] : 0) => arr.reduce((a, c) => a + c, i)
+const adder = (arr, i) => arr.reduce((a, c) => a + c, i ?? 0)
 
-function sumOrMul(arr, i = arr.length > 0 ? arr[0] : 0) {
+function sumOrMul(arr, i ) {
 
-    const out = arr.reduce((a, c) => c % 2 == 0 ? a * c : a + c, i)
+    const out = arr.reduce((a, c) => c % 2 == 0 ? a * c : a + c, i??0)
     return out
 }
 
 
-const funcExec = (arr, i = arr.length > 0 ? arr[0] : 0) => arr.reduce((a, c) => c(a), i)
+const funcExec = (arr, i) => arr.reduce((a, c) => c(a), i??0)
