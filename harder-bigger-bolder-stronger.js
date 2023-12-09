d1092874d0a67043a0b8869b7ae23986265f55c4
aelkarou <karoumi14@gmail.com>
2023-12-09 10:43:15 +0100

    bigger

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,12 @@
+const generateLetters = () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for (let i = 1; i <= 120; i++) {
+        const char = characters[Math.floor(Math.random() * characters.length)]
+        const div = document.createElement('div')
+        div.textContent = char
+        div.style.fontSize = `${i + 10}`
+        div.style.fontWeight = i > 40 ? i > 80 ? '600' : '400' : '300'
+        document.body.append(div)
+    }
+}
+export{generateLetters}
