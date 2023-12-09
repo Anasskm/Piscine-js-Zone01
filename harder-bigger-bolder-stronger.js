b2837515dac754e3436ebd389459796336dc84f8
aelkarou <karoumi14@gmail.com>
2023-12-09 10:46:32 +0100

    bigger

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -4,7 +4,7 @@ const generateLetters = () => {
         const char = characters[Math.floor(Math.random() * characters.length)]
         const div = document.createElement('div')
         div.textContent = char
-        div.style.fontSize = `${i + 10}`
+        div.style.fontSize = `${i + 10}px`
         div.style.fontWeight = i > 40 ? i > 80 ? '600' : '400' : '300'
         document.body.append(div)
     }
